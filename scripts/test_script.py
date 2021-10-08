1b617f5344729b5aa48c3c003d07c1ce17ad75e7
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-10-08 15:50:13 -0400

    Add logging test and `black`en script

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -1,17 +1,26 @@
+import logging
+
+logging.basicConfig(
+    format="%(asctime)-15s %(levelname)s %(message)s", level=logging.INFO
+)
+
 group_outputs = {
-  "Testing log grouping": "This should be in a group.",
-  "Second log group": "Hello, world!",
-  "Incoming Message": "We require more vespene gas!",
-  "mcdonnnj/best-repo-ever": "Simply the best. Better than all the rest.",
+    "Testing log grouping": "This should be in a group.",
+    "Second log group": "Hello, world!",
+    "Incoming Message": "We require more vespene gas!",
+    "mcdonnnj/best-repo-ever": "Simply the best. Better than all the rest.",
 }
 
 
 print("Beginningr output.")
 
 for group_name, group_message in group_outputs.items():
-  print(f"::group::{group_name}")
-  print(group_message)
-  print("::endgroup::")
+    print(f"::group::{group_name}")
+    print(group_message)
+    print("::endgroup::")
+
+logging.info("::group::Logging test")
+logging.info("Hello, world!")
+logging.info("::endgroup::")
 
 print("Finishing output.")
-
