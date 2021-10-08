9d6554afee5071cb3bf214e1f5efdc82b840eabc
Nicholas McDonnell <50747025+mcdonnnj@users.noreply.github.com>
2021-10-08 15:24:29 -0400

    Add additional log groups to test

--- scripts/test_script.py
+++ scripts/test_script.py
@@ -1,9 +1,16 @@
+group_outputs = {
+  "Testing log grouping": "This should be in a group.",
+  "Second log group": "Hello, world!",
+  "Incoming Message": "We require more vespene gas!"
+}
+
 
 print("Beginningr output.")
 
-print("::group::Testing log grouping")
-print("This should be in a group.")
-print("::endgroup::")
+for group_name, group_message in group_outputs.items():
+  print(f"::group::{group_name}")
+  print(group_message)
+  print("::endgroup::")
 
 print("Finishing output.")
 
